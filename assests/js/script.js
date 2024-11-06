abe8bc076f3db656e64aea7618e9447b386f589e
Daniel Anderson <danandwords@outlook.com>
2024-11-06 11:50:49 +0000

    Add code to calculate two random numbers for the game quesitons

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,3 +1,26 @@
+// Wait for the DOM to finish loading before running the game
+
+
+// Get the button elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", function() {
+    // Add the buttons into an array that can be iterrated through
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            // "this" refers to the button that was just clicked, getAttribute will then check the data type content of that button.
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit");
+            }
+            else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+
 function runGame() {
 
 }
@@ -27,5 +50,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
