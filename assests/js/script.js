7703ef8951a03e9d990f5097910256a30e09c449
Daniel Anderson <danandwords@outlook.com>
2024-11-07 13:15:14 +0000

    Add division section and functionality

--- assests/js/script.js
+++ assests/js/script.js
@@ -52,6 +52,9 @@ function runGame(gameType) {
     else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
     }
+    else if (gameType === "division") {
+        displayDivideQuestion(num1, num2);
+    }
         
     else {
         alert(`Unknown game type: ${gameType}`);
@@ -96,7 +99,10 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     }
     else if (operator === "-") {
-        return [operand1 - operand2, "subtract"]
+        return [operand1 - operand2, "subtract"];
+    }
+    else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -123,6 +129,7 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
+
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "+";
@@ -137,8 +144,18 @@ function displaySubtractQuestion(operand1, operand2) {
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
+
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
 
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    operand1 = operand1 * operand2;
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
+    
 }
